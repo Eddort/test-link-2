57bb16b8cf4ddb06382d4d923992df1ed3948a59
Alexander <webisforce@gmail.com>
2020-10-08 17:04:38 +0300

    Some changes

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5f7f1c3a062e35001f19ea39",
+				"5f7f1c3a062e35001f19ea3b"
+			]
+		},
+		"5f7f1c3a062e35001f19ea39": {
+			"id": "5f7f1c3a062e35001f19ea39",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5f7f1c3a062e35001f19ea3b": {
+			"id": "5f7f1c3a062e35001f19ea3b",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
